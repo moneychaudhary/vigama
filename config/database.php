794e9b66f707369ada92be6b5d21bca94dec5c9c
moneychaudhary5 <moneychaudhary5@gmail.com>
2017-05-15 12:24:57 +0530

    Second Commit1

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'vigama2k17.mysql.us1.frbit.com'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'vigama2k17'),
+            'username' => env('DB_USERNAME', 'vigama2k17'),
+            'password' => env('DB_PASSWORD', 'wmHfvKEqxnZGq+LIGRQ3REgg'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
